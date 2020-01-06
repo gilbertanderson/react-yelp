875e5f8de816515557c1c3aa37bb9c8db1fcd544
Gilbert Anderson <gilbertjanderson@gmail.com>
2020-01-06 14:13:21 -0500

    proj init

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import "../../styles.css";
+import SearchBar from "../SearchBar/SearchBar";
+import BusinessList from "../BusinessList/BusinessList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
